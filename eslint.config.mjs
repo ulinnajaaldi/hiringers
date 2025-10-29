e4191f5015cbb4fa930977559423b375281670fc
ulinnajaaldi <ulinnajaaldi24@gmail.com>
2025-10-29 19:40:56 +0700

    setup♻️: update ESLint conf, enhance Next.js conf, and add more depedencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -16,6 +16,8 @@ const eslintConfig = defineConfig([
   {
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
+      "react-hooks/incompatible-library": "off",
+      "@typescript-eslint/ban-ts-comment": "off",
     },
   },
 ]);
--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   typedRoutes: true,
   reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "bqxbzujwpopfbnuygbgc.supabase.co",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
